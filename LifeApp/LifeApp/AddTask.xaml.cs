5993be225cbe86e357aa2a08f0de360b2357741a
berneymark <berneydwmark@gmail.com>
2018-05-07 22:48:07 +1200

    added missing file

---
+++ LifeApp/LifeApp/AddTask.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LifeApp
+{
+    public partial class AddTask : ContentPage
+    {
+        public AddTask(string text)
+        {
+            InitializeComponent();
+
+            Entry editTaskEntry = this.FindByName<Entry>("editTaskEntry");
+            editTaskEntry.Text = text;
+        }
+    }
+}
