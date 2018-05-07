63739531ab1c9cec4b444af8ab1c243ad0c60f8b
WilsonLiu25 <32662781+WilsonLiu25@users.noreply.github.com>
2018-05-07 23:53:15 +1200

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LifeApp.w1
+{
+	public partial class MainPage : TabbedPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            this.BindingContext = new[] { "a", "b", "c" };
+
+        }
+
+        static string bomb = new Random().Next(1, 4).ToString();
+
+        async void ButtonClicked(object userButton, EventArgs e)
+        {
+            Button button = userButton as Button;
+
+            if (button.Text == bomb) //game over
+            {
+                await DisplayAlert("Bomb exploded", "Game Over", "Retry");
+                bomb = new Random().Next(1, 4).ToString();
+            }
+
+        }
+
+        void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e == null) return; // has been set to null, do not 'process' tapped event
+            Console.WriteLine("Tapped: " + e.Item);
+            ((ListView)sender).SelectedItem = null; // de-select the row
+        }
+    }
+}
---
+++ Page3.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+    
+namespace LifeApp.w1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page3 : ContentPage
+	{
+		public Page3 ()
+		{
+			InitializeComponent ();
+
+            this.BindingContext = new[] { "a", "b", "c" };
+        }
+
+        void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e == null) return; // has been set to null, do not 'process' tapped event
+            Console.WriteLine("Tapped: " + e.Item);
+            ((ListView)sender).SelectedItem = null; // de-select the row
+        }
+    }
+}
