1b5f013995dfd2bddca4df313ff95a7d41bb0c10
WilsonLiu25 <will.25@live.com.au>
2018-05-19 15:06:15 +1200

    Users intial

--- LifeApp/LifeApp/MainPage.xaml.cs
+++ LifeApp/LifeApp/MainPage.xaml.cs
@@ -9,25 +9,44 @@ namespace LifeApp
 {
     public partial class MainPage : ContentPage
     {
-        public MainPage()
-        {
-            InitializeComponent();
-
+        public class UserInfo
+        { 
+            public string userName { get; set; }
+            public int level { get; set; }
         }
+
+        private void setTestUser()
+        {
+            var testUser = new UserInfo();
+            testUser.userName = "Calvin";
+            testUser.level = 3;
+        }
+        
         async void OnButtonClicked(object sender, EventArgs args)
         {
+            setTestUser();
+
+
             Label Level = this.FindByName<Label>("Level");
-            Convert.ToInt32(Level.Text);
+            int convertLevelInt = Convert.ToInt32(Level.Text);
+            //convertLevelInt = testUser.level;
+            String convertLevelString = Convert.ToString(convertLevelInt);
+            Level.Text = convertLevelString;
             Label User = this.FindByName<Label>("User");
             ProgressBar MainLevel = this.FindByName<ProgressBar>("MainLevel");
             await MainLevel.ProgressTo(1, 250, Easing.Linear);
             User.Text = "Level up";
+            //testUser.level++;
             await MainLevel.ProgressTo(0, 250, Easing.Linear);
             User.Text = "Name";
-
         }
 
+        public MainPage()
+        {
+            InitializeComponent();
+            setTestUser();
+        }
     }
-
-
 }
+
+
