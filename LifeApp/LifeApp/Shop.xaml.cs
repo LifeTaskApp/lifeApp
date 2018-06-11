4205e7aaa88500bcdec0a1d78c4dd94b60a814bc
Mussab Shireh <mussabshireh@gmail.com>
2018-06-11 23:24:49 +1200

    final

--- LifeApp/LifeApp/Shop.xaml.cs
+++ LifeApp/LifeApp/Shop.xaml.cs
@@ -7,84 +7,122 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace LifeApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Shop : TabbedPage
     {
-        private int itemChoosen = 0;
-        //   public List<newItem> myItems { get; set; }
-        ObservableCollection<newItem> itemList = new ObservableCollection<newItem>();
+        public int itemChoosen = 0;
 
         public Shop()
         {
             InitializeComponent();
 
             ViewModel vm;
-
             vm = new ViewModel();
-            MainListView.ItemsSource = vm.Tnewitem;
+            MainListView.ItemsSource = vm.Currentnewitem;
+
+           // MainListView.BindingContext = itemList;
         }
 
         public class ViewModel
         {
-            public List<newItem> Tnewitem { get; set; }
+            public ObservableCollection<newItem> Currentnewitem { get; set; }
 
             public ViewModel()
             {
-                Tnewitem = new newItem().GetItems();
+                Currentnewitem = new newItem().GetItems();
             }
         }
-   
-
-    // This class makes a new Item
-    public class newItem
+        // This class makes a new Item
+        public class newItem
         {
+
             public String Name { get; set; }
             public int Cost { get; set; }
             public ImageSource Img { get; set; }
             public Boolean Equipped { get; set; }
-      
-            //public newItem(string Name, int Cost, Boolean Equipped/*, String image*/)
-            //{
-            //    this.Name = Name;
-            //    this.Cost = Cost;
-            //    this.Equipped = Equipped;
-            //   // this.Img = ImageSource.FromFile(image);
-                
-            //}
 
-            public List<newItem> GetItems()
+            ObservableCollection<newItem> items = new ObservableCollection<newItem>(); //items variable of observableCollection
+
+
+            //data encapsualtion set method
+            public ObservableCollection<newItem> GetItems()
             {
-                List<newItem> items = new List<newItem>()
-                {
-                    new newItem(){ Name = "Orange-T", Cost = 50, Equipped = false},
-                    new newItem(){ Name = "Red-T", Cost = 100, Equipped = false}
-                };
+                //    items.Add(new newItem { Name = this.Name, Cost = this.Cost, Equipped = this.Equipped});
+                //    items.Add(new newItem { Name = "Orange-T", Cost = 34, Equipped = false });
 
                 return items;
+
             }
 
-
-
+            //data encapsualtion set method 
+            public void SetItems(string myName, int myCost, Boolean myEquipped)
+            {
+                items.Add(new newItem()
+                {
+                    Name = myName,
+                    Cost = myCost,
+                    Equipped = myEquipped
+                });
+            }
         }
 
         //Method to add item bought to list
-        public void InInventory()
+        public void AddToInventory()
         {
+            newItem addItem = new newItem();
             //Items inorder to populate the ListView.
             if (itemChoosen == 1)
             {
+                addItem.SetItems("Orange-Shoes", 50, false);
+                
             }
             else if (itemChoosen == 2)
             {
+                addItem.SetItems("Red-Shoes", 50, false);
             }
             else if (itemChoosen == 3)
             {
+                addItem.SetItems("Blue-Shoes", 50, false);
             }
             else if (itemChoosen == 4)
             {
+                addItem.SetItems("Green-Shoes", 50, false);
+            }
+            else if (itemChoosen == 5)
+            {
+                addItem.SetItems("Orange-T", 50, false);
+            }
+            else if (itemChoosen == 6)
+            {
+                addItem.SetItems("Red-T", 50, false);
+            }
+            else if (itemChoosen == 7)
+            {
+                addItem.SetItems("Blue-T", 50, false);
+            }
+            else if (itemChoosen == 8)
+            {
+                addItem.SetItems("Green-T", 50, false);
+            }
+            else if (itemChoosen == 9)
+            {
+                addItem.SetItems("Orange-Pants", 50, false);
+            }
+            else if (itemChoosen == 10)
+            {
+                addItem.SetItems("Red-Pants", 50, false);
+            }
+            else if (itemChoosen == 11)
+            {
+                addItem.SetItems("Blue-Pants", 50, false);
+            }
+            else if (itemChoosen == 12)
+            {
+                addItem.SetItems("Green-Pants", 50, false);
             }
 
         }
@@ -92,6 +130,7 @@ namespace LifeApp
         async void ButtonClicked(object sender, EventArgs e)
         {
             Button button = sender as Button;
+        //    newItem addItem = new newItem();
 
             //Check Button Clicked
             if (button.StyleId == "1")
@@ -102,9 +141,13 @@ namespace LifeApp
                 //creates pop up to prompt if customer wants to buy item and save it in inventory.
                 if (buy == "Buy")
                 {
-                    await DisplayAlert("Item 1", "Saved in inventory", "Cancel");
-                    itemChoosen = 1; // Item which was bought by the user.
+                    itemChoosen = 1;
+                    bool doBuy = await DisplayAlert("Item 1", "Saved in inventory", "Okay", "Cancel");
 
+                    if (doBuy)
+                    {
+                        AddToInventory();
+                    }
                 }
             }
             else if (button.StyleId == "2")
@@ -113,8 +156,13 @@ namespace LifeApp
 
                 if (buy == "Buy")
                 {
-                    await DisplayAlert("Item 2", "Saved in inventory", "Cancel");
-                    itemChoosen = 2; 
+                    itemChoosen = 2;
+                    bool doBuy = await DisplayAlert("Item 2", "Saved in inventory", "Okay", "Cancel");
+
+                    if (doBuy)
+                    {
+                        AddToInventory();
+                    }
                 }
             }
             else if (button.StyleId == "3")
@@ -125,6 +173,7 @@ namespace LifeApp
                 {
                     await DisplayAlert("Item 3", "Saved in inventory", "Cancel");
                     itemChoosen = 3;
+             //       addItem.SetItems("Blue-Shoes", 50, false);
                 }
             }
             else if (button.StyleId == "4")
@@ -135,6 +184,87 @@ namespace LifeApp
                 {
                     await DisplayAlert("Item 4", "Saved in inventory", "Cancel");
                     itemChoosen = 4;
+            //        addItem.SetItems("Green-Shoes", 50, false);
+                }
+            }
+            else if (button.StyleId == "5")
+            {
+                String buy = await DisplayActionSheet("Selected Item 1", "Cancel", null, "Buy");
+
+                if (buy == "Buy")
+                {
+                    await DisplayAlert("Item 1", "Saved in inventory", "Cancel");
+                    itemChoosen = 5;
+                }
+            }
+            else if (button.StyleId == "6")
+            {
+                String buy = await DisplayActionSheet("Selected Item 2", "Cancel", null, "Buy");
+
+                if (buy == "Buy")
+                {
+                    await DisplayAlert("Item 2", "Saved in inventory", "Cancel");
+                    itemChoosen = 6;
+                }
+            }
+            else if (button.StyleId == "7")
+            {
+                String buy = await DisplayActionSheet("Selected Item 3", "Cancel", null, "Buy");
+
+                if (buy == "Buy")
+                {
+                    await DisplayAlert("Item 3", "Saved in inventory", "Cancel");
+                    itemChoosen = 7;
+                }
+            }
+            else if (button.StyleId == "8")
+            {
+                String buy = await DisplayActionSheet("Selected Item 4", "Cancel", null, "Buy");
+
+                if (buy == "Buy")
+                {
+                    await DisplayAlert("Item 4", "Saved in inventory", "Cancel");
+                    itemChoosen = 8;
+                }
+            }
+            else if (button.StyleId == "9")
+            {
+                String buy = await DisplayActionSheet("Selected Item 1", "Cancel", null, "Buy");
+
+                if (buy == "Buy")
+                {
+                    await DisplayAlert("Item 1", "Saved in inventory", "Cancel");
+                    itemChoosen = 9;
+                }
+            }
+            else if (button.StyleId == "10")
+            {
+                String buy = await DisplayActionSheet("Selected Item 2", "Cancel", null, "Buy");
+
+                if (buy == "Buy")
+                {
+                    await DisplayAlert("Item 2", "Saved in inventory", "Cancel");
+                    itemChoosen = 10;
+                }
+            }
+            else if (button.StyleId == "11")
+            {
+                String buy = await DisplayActionSheet("Selected Item 3", "Cancel", null, "Buy");
+
+                if (buy == "Buy")
+                {
+                    await DisplayAlert("Item 3", "Saved in inventory", "Cancel");
+                    itemChoosen = 11;
+                }
+            }
+            else if (button.StyleId == "12")
+            {
+                String buy = await DisplayActionSheet("Selected Item 4", "Cancel", null, "Buy");
+
+                if (buy == "Buy")
+                {
+                    await DisplayAlert("Item 4", "Saved in inventory", "Cancel");
+                    itemChoosen = 12;
                 }
             }
 
